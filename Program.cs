eca8f96b3c9e0ebcb5871f62b9555f3ad16e473b
Kedrorex <belousovvk@bk.ru>
2023-05-23 16:37:50 +0300

    start inic

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+void GudStringTender (string path, string text)
+{
+    
+    if (File.Exists(path))
+    {
+        File.AppendAllText(path, text);
+    }
+    else
+    {
+        Console.WriteLine("No fale");
+    }
+
+}
+
+Console.WriteLine("Введите компанию: ");
+string company =  Convert.ToString(Console.ReadLine());
+
+string trak = @"C:\Users\Айдар\Desktop\1\C#\Ishod_code\123\catalog\tender.txt";
+string tols = "\nИсходящий №";
+
+int numberLine = System.IO.File.ReadAllLines(trak).Length + 1;
+
+
+
+tols = tols + " " + numberLine + " " + "от " + DateTime.Now + " " + "\"" + company + "\"" ;
+Console.WriteLine(DateTime.Now);
+
+
+
+
+// Console.WriteLine(NumberLine(trak));
+GudStringTender(trak, tols);
+Console.WriteLine("Добавлена новая запись: ");
+Console.WriteLine(tols);
